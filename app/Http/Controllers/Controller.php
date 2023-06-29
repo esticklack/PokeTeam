126c96caa076d1641f381ab919dfc3aba6323868
Lucaso <alexanderviveros9@gmail.com>
2023-06-28 23:11:33 -0400

    Revert "Update Controller.php"
    This reverts commit b3fff707caea5f3ef8ddb7f34ebba2b109a875b1.

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,12 +10,3 @@ class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
 }
-use App\Http\Controllers\Auth\LoginController;
-use App\Http\Controllers\Auth\RegisterController;
-
-Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
-Route::post('/login', [LoginController::class, 'login']);
-Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
-
-Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('register');
-Route::post('/register', [RegisterController::class, 'register']);
